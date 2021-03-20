ca953f870115f4444a15bac5a2ba529ca87733dc
boringdao <boringdao@163.com>
2021-03-20 17:10:48 +0800

    fix: version in gorelease

--- cmd/bridge/version.go
+++ cmd/bridge/version.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
-	"github.com/boringdao/bridge"
 	"github.com/urfave/cli"
+)
+
+var (
+	// current project version
+	version = ""
+	// current git commit hash
+	commit = ""
+	// compile date
+	date = ""
+	// GoVersion system go version
+	GoVersion = runtime.Version()
+	// Platform info
+	Platform = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
 )
 
 var versionCMD = cli.Command{
@@ -18,11 +31,11 @@ var versionCMD = cli.Command{
 }
 
 func getVersion(all bool) string {
-	version := fmt.Sprintf("bridge-node version: %s-%s\n", bridge.CurrentVersion, bridge.CurrentCommit)
+	version := fmt.Sprintf("Bridge version: %s-%s\n", version, commit)
 	if all {
-		version += fmt.Sprintf("App build date: %s\n", bridge.BuildDate)
-		version += fmt.Sprintf("System version: %s\n", bridge.Platform)
-		version += fmt.Sprintf("Golang version: %s\n", bridge.GoVersion)
+		version += fmt.Sprintf("App build date: %s\n", date)
+		version += fmt.Sprintf("System version: %s\n", Platform)
+		version += fmt.Sprintf("Golang version: %s\n", GoVersion)
 	}
 
 	return version
